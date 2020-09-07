12f464a20b28d0ff70ea7b0036ea89796a76d60a
rodds93 <rodrigodds96@gmail.com>
2020-09-07 19:13:44 -0300

    Inclusão da classe disponibilidade

---
+++ src/main/java/br/com/etechoracio/monitoria/enums/DiaSemanaEnum.java
@@ -0,0 +1,11 @@
+package br.com.etechoracio.monitoria.enums;
+
+public enum DiaSemanaEnum {
+	SEGUNDA_FEIRA,
+	TERCA_FEIRA,
+	QUARTA_FEIRA,
+	QUINTA_FEIRA,
+	SEXTA_FEIRA;
+	
+
+}
---
+++ src/main/java/br/com/etechoracio/monitoria/model/Disponibilidade.java
@@ -0,0 +1,51 @@
+package br.com.etechoracio.monitoria.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.etechoracio.monitoria.enums.DiaSemanaEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_DISPONIBILIDADE")
+public class Disponibilidade {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ID_DISPONIBILIDADE")
+	private Long id;
+	
+	@Enumerated(EnumType.STRING) /* Como é uma enumeração é necessario declarar @enumerated, Como no banco de dados o tipo
+	da variavel é varchar é necessario avisar o que seria do tipo texto ou seja string*/
+	@Column(name = "TX_DIA_SEMANA")
+	private DiaSemanaEnum diaSemanaEnum;
+	
+	@Temporal(TemporalType.TIME)
+	@Column(name = "HR_INICIO")
+	private Date horaInicio;
+	
+	@Temporal(TemporalType.TIME)
+	@Column(name = "HR_FIM")
+	private Date horaFim;
+	
+	@ManyToOne
+	@JoinColumn(name = "ID_USUARIO")/*Join Column é utilizado quando existe um relacionamento entre tabelas */
+	private Usuario usuario; /*Existem 4 tipos de relacionamento para FK OneToOne,OneToMany,ManyToOne
+	 e ManytoMany*/
+	
+
+}
